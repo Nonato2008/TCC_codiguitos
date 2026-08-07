0a105640b69f105aca976d14e64c2f65310c1c3c
MIGUEL HENRI GOMES NUNES <miguel.nunes9@senaisp.edu.br>
2026-08-07 15:14:58 -0300

    alterar status

--- BACK-END/src/controllers/produtoController.js
+++ BACK-END/src/controllers/produtoController.js
@@ -2,6 +2,23 @@ import { statusPed } from "../enums/statusVenda.js";
 import { Produtos } from "../models/Produtos.js";
 import produtosRepository from "../repositories/produtosRepository.js";
 
+const calcularStatus = (quantidade, dataVenc) => {
+
+    const hoje = new Date();
+    hoje.setHours(0, 0, 0, 0);
+
+    const vencimento = new Date(dataVenc);
+    vencimento.setHours(0, 0, 0, 0);
+
+    if (vencimento < hoje) {
+        return statusPed.VENCIDO;
+    }
+    if (Number(quantidade) <= 0) {
+        return statusPed.ESGOTADO;
+    }
+    return statusPed.ESTOQUE;
+};
+
 const produtoController = {
     inserir: async (req, res) => {
         try {
@@ -22,16 +39,34 @@ const produtoController = {
     alterar: async (req, res) => {
         try {
             const id = req.params.id;
-            const { idFornecedor, nome, preco, quantidade, status, dataVenc } = req.body;
-          
-            const imagem = req.file ? `/uploads/imagens/${req.file.filename}` : null;
-           const produto = Produtos.alterar({ idFornecedor, nome, preco, quantidade, status, imagem, dataVenc }, id);
+
+            const {
+                idFornecedor, nome, preco, quantidade, dataVenc
+            } = req.body;
+
+            const imagem = req.file
+                ? `/uploads/imagens/${req.file.filename}`
+                : null;
+
+            const status = calcularStatus(
+                quantidade,
+                dataVenc
+            );
+
+            const produto = Produtos.alterar({
+                idFornecedor, nome, preco, quantidade, status, imagem, dataVenc
+            }, id);
+
             const result = await produtosRepository.editar(produto);
             res.status(200).json({ message: 'Produto alterado com sucesso', result });
+            console.log(result)
 
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: 'Erro ao alterar produto', errorMessage: error.message });
+            res.status(500).json({
+                message: "Erro ao alterar produto",
+                errorMessage: error.message
+            });
         }
     },
     deletar: async (req, res) => {
