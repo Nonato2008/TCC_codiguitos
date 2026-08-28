d82414c6bb80ab5f4af60aedf006556ee99dc43c
BRYAN NONATO DA SILVA CARVALHO <bryan.carvalho5@senaisp.edu.br>
2026-08-28 12:18:06 -0300

    botão para acessar a página de estoque

--- FRONT-END/src/components/Sidebar.jsx
+++ FRONT-END/src/components/Sidebar.jsx
@@ -12,6 +12,11 @@ const menu = [
         nome: "Cadastro de Produtos",
         icone: "inventory_2",
         rota: "/cadastroProdutos"
+    },
+    {
+        nome: "Gerenciamento de Estoque",
+        icone: "inventory",
+        rota: "/gerenciamentoEstoque"
     }
 ];
 
