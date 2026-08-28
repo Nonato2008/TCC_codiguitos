0677db42137a2aa397d379cd9632f46c137b04d9
BRYAN NONATO DA SILVA CARVALHO <bryan.carvalho5@senaisp.edu.br>
2026-08-28 11:55:20 -0300

    pagina de cadastro de produtos

--- BACK-END/src/server.js
+++ BACK-END/src/server.js
@@ -1,14 +1,17 @@
-import express from 'express';
-import routes from './routes/routes.js';
-import dotenv from 'dotenv';
+    import express from 'express';
+    import routes from './routes/routes.js';
+    import dotenv from 'dotenv';
+    import cors from "cors";
 
-dotenv.config();
+    dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use('/', routes);
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Servidor rodando em: http://localhost:${process.env.SERVER_PORT}`);
-});
+app.use("/", routes);
+
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`Servidor rodando em: http://localhost:${process.env.SERVER_PORT}`);
+    });
